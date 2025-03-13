457adad44cb0910f66f33c0d5e0616b3a8392800
Tien Dung <dungtn@gmail.com>
2025-03-13 09:41:19 +0000

    update

--- .save/tool_use_messages.js
+++ .save/tool_use_messages.js
@@ -1,19 +1,15 @@
 ```sh
 bun run index.js -p "đếm số dòng code *.js"
 === SENDING PROMPT TO LLM ===
-Messages: [
-  { "role": "user", "content": [ { "type": "text", "text": "đếm số dòng code *.js" } ] },
-]
+Messages: [ { "role": "user", "content": [ { "type": "text", "text": "đếm số dòng code *.js" } ] }]
 === END OF PROMPT ===
 > assistant
 I'll help you count the number of lines of code in JavaScript files.
 I'll use the BashTool to do this with the `wc` (word count) command:
 
 > BashTool: {"command":"find . -name \"*.js\" | xargs wc -l"}
-
 > user
 > toolu_01XMp6AAw37v91vVYKueDvFH: {"stdout":"177 ./file-write.js\n  113 ./api.js\n  100 ./grep.js\n  283 ./bash.js\n  155 ./prompts.js\n  140 ./file-read.js\n  116 ./agent.js\n    7 ./constants.js\n  323 ./persistent_shell.js\n  323 ./file-edit.js\n  143 ./glob.js\n   21 ./index.js\n  200 ./ls.js\n   27 ./test-grep.js\n   56 ./test-persistent_shell.js\n 2184 total"}
-
 
 === SENDING PROMPT TO LLM ===
 Messages: [
@@ -22,15 +18,12 @@ Messages: [
       { "type": "text",
         "text": "I'll help you count the number of lines of code in JavaScript files.\nI'll use the BashTool to do this with the `wc` (word count) command:"
       },
-      { "type": "tool_use",
-        "id": "toolu_01XMp6AAw37v91vVYKueDvFH",
-        "name": "BashTool",
+      { "type": "tool_use", "id": "toolu_01XMp6AAw37v91vVYKueDvFH", "name": "BashTool",
         "input": { "command": "find . -name \"*.js\" | xargs wc -l" }
       }
     ]},
   { "role": "user", "content": [
-      { "type": "tool_result",
-        "tool_use_id": "toolu_01XMp6AAw37v91vVYKueDvFH",
+      { "type": "tool_result", "tool_use_id": "toolu_01XMp6AAw37v91vVYKueDvFH",
         "content": "{\"stdout\":\"177 ./file-write.js\\n  113 ./api.js\\n  100 ./grep.js\\n  283 ./bash.js\\n  155 ./prompts.js\\n  140 ./file-read.js\\n  116 ./agent.js\\n    7 ./constants.js\\n  323 ./persistent_shell.js\\n  323 ./file-edit.js\\n  143 ./glob.js\\n   21 ./index.js\\n  200 ./ls.js\\n   27 ./test-grep.js\\n   56 ./test-persistent_shell.js\\n 2184 total\"}"
       }]
   }]
@@ -52,6 +45,5 @@ Here's a breakdown of JavaScript files and their line counts:
 - ls.js: 200 lines
 - test-grep.js: 27 lines
 - test-persistent_shell.js: 56 lines
-
 Total lines of code: 2,184 lines
 ```
