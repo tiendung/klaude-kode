78602bd44dcdd74ea1a8d5424bde44d95ebb2f57
Tien Dung <dungtn@gmail.com>
2025-03-14 00:26:18 +0000

    refine

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ import { getSystemPrompt } from './prompts.js';
 
 const systemPrompt = await getSystemPrompt();
 
-const userPrompt = process.argv[2] == '-p' ? process.argv.slice(3).join(' ') : // all args after the -p flag
-  "list the files in the current directory"; // default user prompt
+const userPrompt = process.argv[2] == '-p' ? process.argv.slice(3).join(' ') : "count *.js lines"; // koding -p count *.js lines
 
-// Gọi LLM xử lý userPrompt bằng các tools được cung cấp theo hướng dẫn từ systemPrompt 
+// Gọi LLM xử lý userPrompt bằng các tools được cung cấp theo hướng dẫn từ systemPrompt
 await query({ userPrompt, tools, systemPrompt }).catch(error => console.error("Error:", error));
