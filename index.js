d8310cf07143f6cd12ad94bf5b340828cc0c7f6c
Tien Dung <dungtn@gmail.com>
2025-03-14 15:06:57 +0000

    tạm loại bỏ file-edit tool

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ import * as AgentTool from './agent.js';
 import * as BashTool from './bash.js';
 import * as FileReadTool from './file-read.js';
 import * as FileWriteTool from './file-write.js';
-import * as FileEditTool from './file-edit.js';
 import * as GrepTool from './grep.js';
 import * as GlobTool from './glob.js';
 import * as LSTool from './ls.js';
 
-const tools = [AgentTool, BashTool, FileReadTool, FileWriteTool, FileEditTool, GrepTool, GlobTool, LSTool];
+const tools = [AgentTool, BashTool, FileReadTool, FileWriteTool, GrepTool, GlobTool, LSTool];
 
 import { query } from './api.js';
 import { getSystemPrompt } from './prompts.js';
